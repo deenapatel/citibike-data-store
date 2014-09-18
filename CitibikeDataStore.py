b5d83d253b55e6b77f455bfee59d211eec4a19a4
deenapatel <deena@dmpatel.net>
2014-09-17 21:31:58 -0400

    only saves 10k rows per dataframe, then starts over

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -7,21 +7,21 @@ url = "http://citibikenyc.com/stations/json"
 maindf = pd.DataFrame() #this will be the main dataframe that gets appended to.
 updateTime='intial time'
 prevTime='last recorded time'
-count=0
+#count=0
 while dt.date.today().year == 2014:
-    try:
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())
-    except (IOError,ValueError) as e:
-        print 'ERROR:',e,' at time:',updateTime
-    df=json_normalize(data['stationBeanList'])
-    updateTime = data['executionTime']
-    df['time']=updateTime
-    if updateTime != prevTime: #only want to update maindf if the time has changed
-        maindf=maindf.append(df,ignore_index=True)
-        prevTime=updateTime #now prevTime is set to the current updateTime
-        if count%10==0:
-            maindf.to_csv('/home/deena/citibike_data/citibike'+updateTime.replace(' ','-')+'.csv')
-        count += 1
-    time.sleep(60)
-    
+    for count in range(10001):
+        try:
+            response = urllib.urlopen(url)
+            data = json.loads(response.read())
+        except (IOError,ValueError) as e:
+            print 'ERROR:',e,' at time:',updateTime
+        df=json_normalize(data['stationBeanList'])
+        updateTime = data['executionTime']
+        df['time']=updateTime
+        if updateTime != prevTime: #only want to update maindf if the time has changed
+            maindf=maindf.append(df,ignore_index=True)
+            prevTime=updateTime #now prevTime is set to the current updateTime
+            if count%100==0:
+                maindf.to_csv('data/citibike'+updateTime.replace(' ','-')+'.csv')
+            count += 1
+        time.sleep(60)
