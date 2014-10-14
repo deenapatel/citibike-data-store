77e0ab4b12c5a7361c56041713310e547defba16
deenapatel <deena@dmpatel.net>
2014-10-14 12:54:20 -0400

    now appends directly to file on disk at every step

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -1,14 +1,12 @@
 import urllib, json, time
 import pandas as pd
-import datetime as dt
 from pandas.io.json import json_normalize
 
 url = "http://citibikenyc.com/stations/json"
-maindf = pd.DataFrame() #this will be the main dataframe that gets appended to.
 updateTime='intial time'
 prevTime='last recorded time'
-count=0
-while dt.date.today().year == 2014:
+
+while True:
     try:
         response = urllib.urlopen(url)
         data = json.loads(response.read())
@@ -18,10 +16,6 @@ while dt.date.today().year == 2014:
     updateTime = data['executionTime']
     df['time']=updateTime
     if updateTime != prevTime: #only want to update maindf if the time has changed
-        maindf=maindf.append(df,ignore_index=True)
         prevTime=updateTime #now prevTime is set to the current updateTime
-        if count%10==0:
-            maindf.to_csv('/home/deena/citibike_data/citibike'+updateTime.replace(' ','-')+'.csv')
-        count += 1
+     	df.to_csv('data/citibike_data.csv',header=False, mode='a')
     time.sleep(60)
-    
