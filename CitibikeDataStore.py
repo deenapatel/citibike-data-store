2d5f68f32741c0ce11d117e12ed024333da792c4
deenapatel <deena@dmpatel.net>
2014-10-14 15:05:41 -0400

    added the case where no file exists initally

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -1,11 +1,23 @@
-import urllib, json, time
+import urllib, json, time, os.path
 import pandas as pd
 from pandas.io.json import json_normalize
 
 url = "http://citibikenyc.com/stations/json"
-updateTime='intial time'
+updateTime='current time'
 prevTime='last recorded time'
 
+#checks first to see if the file exits
+if not os.path.exists('data/citibike_data.csv'):
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    df=json_normalize(data['stationBeanList'])
+    updateTime = data['executionTime']
+    df['time']=updateTime
+    prevTime=updateTime
+    df.to_csv('data/citibike_data.csv')#writes a new file to disk
+    time.sleep(60)
+    
+#note: this keeps adding on to the file, infinite loop
 while True:
     try:
         response = urllib.urlopen(url)
@@ -15,7 +27,7 @@ while True:
     df=json_normalize(data['stationBeanList'])
     updateTime = data['executionTime']
     df['time']=updateTime
-    if updateTime != prevTime: #only want to update maindf if the time has changed
+    if updateTime != prevTime: #only want to update if the time has changed
         prevTime=updateTime #now prevTime is set to the current updateTime
      	df.to_csv('data/citibike_data.csv',header=False, mode='a')
     time.sleep(60)
