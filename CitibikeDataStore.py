44ea2ad93449b70c965a9b3f79f7c0cb80915fad
deenapatel <deena@dmpatel.net>
2014-11-19 11:53:50 -0500

    added wildcard exception case

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -18,17 +18,19 @@ if not os.path.exists('data/citibike_data.csv'):
     df.to_csv('data/citibike_data.csv')#writes a new file to disk
     time.sleep(60)
     
-#note: this keeps adding on to the file, infinite loop
+#this keeps adding on to the file, infinite loop
 while True:
     try:
         response = urllib.urlopen(url)
         data = json.loads(response.read())
     except (IOError,ValueError) as e:
         print 'ERROR:',e,' at time:',updateTime
+    except:
+        print 'other ERROR at time:',updateTime
     df=json_normalize(data['stationBeanList'])
     updateTime = data['executionTime']
     df['time']=updateTime
     if updateTime != prevTime: #only want to update if the time has changed
-        prevTime=updateTime #now prevTime is set to the current updateTime
+        prevTime=updateTime
      	df.to_csv('data/citibike_data.csv',header=False, mode='a')
     time.sleep(60)
