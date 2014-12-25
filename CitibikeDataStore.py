d7f804fe50caa997a1f50aae385d2647c69f8b13
deenapatel <deena@dmpatel.net>
2014-12-25 13:14:53 -0500

    changed urllib to urllib2 to be able to use timeout arg

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -1,4 +1,4 @@
-import urllib, time, os.path, socket
+import urllib2, time, os.path, socket
 import simplejson as json
 import pandas as pd
 from pandas.io.json import json_normalize
@@ -9,7 +9,7 @@ prevTime='last recorded time'
 
 #checks first to see if the file exits
 if not os.path.exists('data/citibike_data.csv'):
-    response = urllib.urlopen(url, timeout=20)
+    response = urllib2.urlopen(url, timeout=20)
     data = json.loads(response.read())
     df=json_normalize(data['stationBeanList'])
     updateTime = data['executionTime']
@@ -21,19 +21,17 @@ if not os.path.exists('data/citibike_data.csv'):
 #this keeps adding on to the file, infinite loop
 while True:
     try:
-        response = urllib.urlopen(url,timeout=20)
+        response = urllib2.urlopen(url,timeout=20)
         data = json.loads(response.read())
+        df=json_normalize(data['stationBeanList'])
+        updateTime = data['executionTime']
+        df['time']=updateTime
+        if updateTime != prevTime: #only want to update if the time has changed
+            prevTime=updateTime
+            df.to_csv('data/citibike_data.csv',header=False, mode='a')
     except (IOError,ValueError) as e:
         print 'ERROR:',e,' at time:',updateTime
-    except (socket.timeout) as e:
-        print 'Socket timeout ERROR:',e,' at time:',updateTime
     except:
-        print 'other ERROR at time:',updateTime
-       
-    df=json_normalize(data['stationBeanList'])
-    updateTime = data['executionTime']
-    df['time']=updateTime
-    if updateTime != prevTime: #only want to update if the time has changed
-        prevTime=updateTime
-     	df.to_csv('data/citibike_data.csv',header=False, mode='a')
-   time.sleep(60)
+        print 'other ERROR at time:',updateTime   
+
+    time.sleep(60)
