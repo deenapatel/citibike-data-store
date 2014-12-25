a259c8b95867f0b718c68e22b90d12b5a7ed27cd
deenapatel <deena@dmpatel.net>
2014-12-25 12:35:46 -0500

    added in socket timeout. also removed some exceptions so the program can easily be exited

--- CitibikeDataStore.py
+++ CitibikeDataStore.py
@@ -1,4 +1,4 @@
-import urllib, time, os.path
+import urllib, time, os.path, socket
 import simplejson as json
 import pandas as pd
 from pandas.io.json import json_normalize
@@ -9,7 +9,7 @@ prevTime='last recorded time'
 
 #checks first to see if the file exits
 if not os.path.exists('data/citibike_data.csv'):
-    response = urllib.urlopen(url)
+    response = urllib.urlopen(url, timeout=20)
     data = json.loads(response.read())
     df=json_normalize(data['stationBeanList'])
     updateTime = data['executionTime']
@@ -21,21 +21,19 @@ if not os.path.exists('data/citibike_data.csv'):
 #this keeps adding on to the file, infinite loop
 while True:
     try:
-        response = urllib.urlopen(url)
+        response = urllib.urlopen(url,timeout=20)
         data = json.loads(response.read())
     except (IOError,ValueError) as e:
         print 'ERROR:',e,' at time:',updateTime
+    except (socket.timeout) as e:
+        print 'Socket timeout ERROR:',e,' at time:',updateTime
     except:
         print 'other ERROR at time:',updateTime
-    try:    
-	df=json_normalize(data['stationBeanList'])
-    	updateTime = data['executionTime']
-    	df['time']=updateTime
-    	if updateTime != prevTime: #only want to update if the time has changed
-            prevTime=updateTime
-     	    df.to_csv('data/citibike_data.csv',header=False, mode='a')
-   	time.sleep(60)
-    except (IOError,ValueError) as e:
-        print 'ERROR:',e,' at time:',updateTime
-    except:
-        print 'other ERROR at time:',updateTime
+       
+    df=json_normalize(data['stationBeanList'])
+    updateTime = data['executionTime']
+    df['time']=updateTime
+    if updateTime != prevTime: #only want to update if the time has changed
+        prevTime=updateTime
+     	df.to_csv('data/citibike_data.csv',header=False, mode='a')
+   time.sleep(60)
